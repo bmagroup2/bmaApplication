d05a92978302b60ad8654aeadd3d0555cb0900ad
bmagroup2 <projectoop635@gmail.com>
2025-07-20 01:27:57 +0600

    Revert "commit"
    This reverts commit fb4b44237e5213592dda685ec5d0cd6ee4695f19.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module bmasec2.bmaapplication {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens bmasec2.bmaapplication to javafx.fxml;
-    exports bmasec2.bmaapplication;
-}
