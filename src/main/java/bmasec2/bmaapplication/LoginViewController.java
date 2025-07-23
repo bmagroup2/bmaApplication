7c0eea587ff238442349ef85e40e55dbb969a80e
bmagroup2 <projectoop635@gmail.com>
2025-07-24 02:57:14 +0600

    first commit

---
+++ src/main/java/bmasec2/bmaapplication/LoginViewController.java
@@ -0,0 +1,19 @@
+package bmasec2.bmaapplication;
+
+public class LoginViewController
+{
+    @javafx.fxml.FXML
+    private ComboBox loginUserRoleComboBox;
+    @javafx.fxml.FXML
+    private TextField loginUsernameTextfield;
+    @javafx.fxml.FXML
+    private PasswordField loginPasswordTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void signInButtonOnAction(ActionEvent actionEvent) {
+    }
+}
